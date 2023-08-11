38680a648371f9f8dabeab7b36169d2474ec06ee
Alexander Beniaminov <alexander.beniaminov@sifox.com>
2023-08-11 21:58:19 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrOffsetIsNegativ       = errors.New("offset is negative")
+	ErrLimitIsNegativ        = errors.New("limit is negative")
 )
 
+type ProgressBar struct {
+	Total int
+	Max   int
+}
+
+func (pb *ProgressBar) Write(p []byte) (int, error) {
+	n := len(p)
+	pb.Total += n
+	pb.PrintProgress()
+	return n, nil
+}
+
+func (pb ProgressBar) PrintProgress() {
+	persent := pb.Total / pb.Max * 100
+	fmt.Printf("\r%s  %d%%", strings.Repeat("#", persent), persent)
+	fmt.Printf("\rCopied... %d bytes ", pb.Total)
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	min := func(x, y int64) int64 {
+		if x > y {
+			return y
+		}
+		return x
+	}
+
+	if offset < 0 {
+		return ErrOffsetIsNegativ
+	}
+	if limit < 0 {
+		return ErrLimitIsNegativ
+	}
+
+	fromFileStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if !fromFileStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	sourceFileSize := fromFileStat.Size()
+	if offset > sourceFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = sourceFileSize
+	}
+
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	dest, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer dest.Close()
+
+	realLimit := min(limit, sourceFileSize-offset)
+	pb := &ProgressBar{Max: int(realLimit)}
+
+	source.Seek(offset, 0)
+	if _, err := io.CopyN(dest, io.TeeReader(source, pb), realLimit); err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("file from is unsupported", func(t *testing.T) {
+		err := Copy("/", "/tmp/mm.txt", 0, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
+	t.Run("offset is negative", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/mm.txt", -1, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetIsNegativ), "actual err - %v", err)
+	})
+	t.Run("limit is negative", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/mm.txt", 0, -1)
+		require.Truef(t, errors.Is(err, ErrLimitIsNegativ), "actual err - %v", err)
+	})
+	t.Run("offset is large", func(t *testing.T) {
+		fromFileStat, _ := os.Stat("testdata/input.txt")
+		sourceFileSize := fromFileStat.Size()
+		err := Copy("testdata/input.txt", "/tmp/mm.txt", sourceFileSize+1, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
+	t.Run("offset eq file size", func(t *testing.T) {
+		fromFileStat, _ := os.Stat("testdata/input.txt")
+		sourceFileSize := fromFileStat.Size()
+		Copy("testdata/input.txt", "/tmp/mm.txt", sourceFileSize, 0)
+		toFileStat, _ := os.Stat("/tmp/mm.txt")
+		destFileSize := toFileStat.Size()
+		require.Equal(t, 0, int(destFileSize), "wrong dest file size")
+		os.Remove(toFileStat.Name())
+	})
+	t.Run("copy real limit", func(t *testing.T) {
+		fromFileStat, _ := os.Stat("testdata/input.txt")
+		sourceFileSize := fromFileStat.Size()
+		Copy("testdata/input.txt", "/tmp/mm.txt", sourceFileSize-2, 0)
+		toFileStat, _ := os.Stat("/tmp/mm.txt")
+		destFileSize := toFileStat.Size()
+		require.Equal(t, 2, int(destFileSize), "wrong dest file size")
+		os.Remove(toFileStat.Name())
+	})
 }
